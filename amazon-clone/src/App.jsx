2033d8768e77d7d73fe7ddfbaf7b1004c2fa28e3
Netsanet Dagnachew <netsishibeshi@gmail.com>
2024-12-01 14:01:43 +0300

    carousel component is done

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -1,11 +1,13 @@
+import React from "react"
 import Header from "./components/Header/Header"
-
+import CarouselEffect from "./components/Carousel/CarouselEffect"
 
 function App() { 
 
   return (
     <>
       <Header />
+      <CarouselEffect />
     </>
   )
 }
